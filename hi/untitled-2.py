6b8f3b867376df2675942ca454e0c1ddfb8c45cf
Timur_VLD <94624695+Timur2030@users.noreply.github.com>
2021-12-20 18:59:59 +0300

    Delete hi directory

--- hi/untitled-2.py
+++
@@ -1,8 +0,0 @@
-total = 0
-print('Вводите цены; для остановки введите -1.')
-price = float(input())
-while price > -1:
-    total = total + price  # можно заменить на аналогичную запись
-    # total += price
-    price = float(input())
-print('Общая стоимость равна', total)
